d39130c407c8a6e6fcabeb09e7f304302e9ea724
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-09-05 17:20:30 -0300

    Exercicio 7 Resolvido

---
+++ src/exercicios_aula07/exercicio7.java
@@ -0,0 +1,30 @@
+package exercicios_aula07;
+import java.util.Scanner;
+
+public class exercicio7 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int nota = 0;
+		int maior = 0;
+		
+		System.out.println("Quantos alunos são na turma?");
+		int alunos = scanner.nextInt();
+		
+		System.out.println("Qual é a média da turma?");
+		int media = scanner.nextInt();
+		
+		
+		
+		for (int i = 0; i<= alunos; i++) {
+			System.out.println("Coloque a nota: ");
+			nota = scanner.nextInt();
+			
+			if (nota >= media) {
+				maior ++;
+			}
+			
+		}
+		System.out.printf("A quantidade de alunos que estão na média são: %d", maior);
+	}
+	
+}
