ee906d29228f4006b991b61eb813adc96ea8db6e
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-10-07 23:12:35 -0300

    Exercicio 1 e 2 da aula 10

---
+++ src/exercicios_aula9/exercicio2.java
@@ -0,0 +1,17 @@
+package exercicios_aula9;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+        String s = "FIAPTOKIO";
+        char[] r = new char[9];
+        for (int i = s.length() - 1; i >= 0; i--) {
+        r[i] = s.charAt(i);
+        }
+        for (char letra: r) {
+        System.out.print(letra);
+        }
+    }
+
+    // A saída é FIAPTOKIO
+    
+}
